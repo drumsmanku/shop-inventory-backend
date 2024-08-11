00d6e6a960f6d9895fdac9321f61dd3aeedf10ce
drumsmanku <mayank.tanwar4444@gmail.com>
2024-08-11 20:36:48 +0530

    updated

--- app.js
+++ app.js
@@ -6,20 +6,17 @@ const dotenv=require('dotenv');
 
 const app = express();
 
-// Connect to the database
-connectDB();
 dotenv.config()
+connectDB();
 
-// Middleware
-app.use(cors()); // Enable CORS for all routes
-app.use(bodyParser.json()); // Parse JSON bodies
-app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(cors()); 
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Routes
 app.use('/inventory', require('./routes/inventoryRoutes'));
 app.use('/bills', require('./routes/billRoutes'));
 
-// Server listening on port 3000
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
